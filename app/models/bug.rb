020c366ab4a6f3e25a96b0c35cf270efb4a63749
kinifi <chrisfigueroa16@gmail.com>
2017-02-01 19:03:33 -0800

    removed the label and model

--- app/models/bug.rb
+++
@@ -1,6 +0,0 @@
-class Bug < ApplicationRecord
-  has_many :labels
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :rating, presence: true
-end
--- app/models/label.rb
+++
@@ -1,4 +0,0 @@
-class Label < ApplicationRecord
-  belongs_to :bug
-  validates :name, presence: true
-end
